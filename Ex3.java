b974cae11fddf580401486a753f5c0f5dd649cb9
NikitaKulakov <91449546+NikitaKulakov@users.noreply.github.com>
2022-06-21 09:29:29 +0300

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,18 +1,18 @@
 /*Определить матрицу и ее заполнить случайными значениями. Построить вектор B, который
 возвращает количество четных чисел в i-й строке.*/
 public class Ex3 {
-    public static int rows = 4;		//кол-во строк в матрице
-    public static int columns = 4;	//кол-во столбцов в матрице
-    public static int min = 1;		//минимальное значение элементов матрицы
-    public static int max = 9;		//максимальное значение элементов матрицы
-    public static int i = 3;		//заданное значение i (не должно быть больше rows)
+    public static int rows = 4;
+    public static int columns = 4;
+    public static int min = 1;
+    public static int max = 9;
+    public static int i = 3;
 
     public static void main(String[] args) {
         i--;	//перевод номера в индекс
         int[][] mat = Ex3.createMatrix(Ex3.rows, Ex3.columns, Ex3.min, Ex3.max);
         int[] B = Ex3.createVector(mat, Ex3.i);
         Ex3.printVector(B);
-        System.out.println("Количество четных числе в i-й строке: " + B.length);
+        System.out.println("Amount of even numbers in i-th line: " + B.length);
     }
 
     public static int[][] createMatrix(int rows, int columns, int min, int max) {
